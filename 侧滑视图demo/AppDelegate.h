0907b2ce434e5b7502ee303e35f6c385a3a5c652
杨华 <593283083@qq.com>
2015-06-25 10:48:34 +0800

    Initial Commit

---
+++ 侧滑视图demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  侧滑视图demo
+//
+//  Created by xxzx on 15/6/25.
+//  Copyright (c) 2015年 hxyxt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 侧滑视图demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  侧滑视图demo
+//
+//  Created by xxzx on 15/6/25.
+//  Copyright (c) 2015年 hxyxt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
